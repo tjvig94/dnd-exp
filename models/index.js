eb363d4cd25152d89b5d881918def5ac235c84aa
Joe Lyons <josephjlyons90@gmail.com>
2021-03-25 16:00:52 -0400

    various commits for routing and models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,40 @@
 const Character = require('./Character');
 const Class = require('./Class');
 const Race = require('./Race');
-const User = require('./User')
+const User = require('./User');
 
 //need to figure out how and what relates to eachother (hasmany etc...)
+
+//Need to rework 
+
+User.hasMany(Character, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade'
+}),
+
+Character.belongsTo(User, {
+    foreignKey: 'user_id'
+}),
+
+// Character.hasMany(Race)
+
+
+//  Race.belongsToMany(Character, {
+//      through: Race,
+//      foreignKey: 'race_id'
+//  })
+
+//  Character.hasMany(Class, {
+//      foreignKey: 'class_id'
+//  })
+
+ 
+
+
 
 module.exports = {
     Class,
     Race, 
     Character,
     User
-};
+}
