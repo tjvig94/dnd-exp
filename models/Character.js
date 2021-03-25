c014b75f7a75b58d1e217b9ed6dc1da3a33d12dc
Joe Lyons <josephjlyons90@gmail.com>
2021-03-24 21:19:12 -0400

    working on models and routes

---
+++ models/Character.js
@@ -0,0 +1,104 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Character extends Model { }
+
+Character.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        race: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: { model: 'race', key: 'id' }
+
+        },
+
+        class: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: { model: 'class', key: 'id' }
+
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: { model: 'user', key: 'id' }
+
+        },
+
+        stats: {
+            type: DataTypes.INTEGER
+        },
+
+        modifiers: {
+            type: DataTypes.INTEGER
+        },
+
+        skills: {
+            type: DataTypes.INTEGER
+        },
+
+        armourclass: {
+            type: DataTypes.INTEGER
+        },
+
+        initiative: {
+            type: DataTypes.INTEGER
+        },
+
+        speed: {
+            type: DataTypes.INTEGER
+        },
+
+        hitpoints: {
+            type: DataTypes.INTEGER
+        },
+
+        hitdice: {
+            type: DataTypes.INTEGER
+        },
+
+        weapon: {
+            type: DataTypes.STRING
+        },
+
+        languages: {
+            type: DataTypes.STRING
+        },
+
+        proficiencies: {
+            type: DataTypes.TEXT
+        },
+
+        equipment: {
+            type: DataTypes.TEXT
+        },
+
+        traits: {
+            type: DataTypes.TEXT
+        }
+
+
+
+    },
+
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'character'
+    }
+
+)
+
+module.exports = Character;
---
+++ models/index.js
@@ -0,0 +1,13 @@
+const Character = require('./Character');
+const Class = require('./Class');
+const Race = require('./Race');
+const User = require('./User')
+
+//need to figure out how and what relates to eachother (hasmany etc...)
+
+module.exports = {
+    Class,
+    Race, 
+    Character,
+    User
+};
