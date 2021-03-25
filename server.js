fcd2216fc4c560f70d4163db5ecb4b7998b87fee
Tom Fallon <tomfallon9@gmail.com>
2021-03-25 19:02:14 -0400

    changed functionality to get home page rendered

--- server.js
+++ server.js
@@ -1,41 +1,33 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+//Dependencies
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require("path");
 
+const db = require("./models");
+
+//Created Server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
-
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
-
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
+//Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-app.use(routes);
+//Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+
+//HTML Routes
+app.get("/", (req, res) => res.render("home"));
+
+
+//Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//Server listener
+
+  app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+
