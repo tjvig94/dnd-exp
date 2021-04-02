0dad32e717a01f37c8c62e2f208b1077e2d7b6aa
Chad Courtney <Chadcourtney567@gmail.com>
2021-04-01 21:10:59 -0400

    have the working log in/log out feature

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -18,6 +18,7 @@ router.post('/register', async (req, res) => {
       res.status(200).json(userData);
     });
   } catch (err) {
+    console.log(err)
     res.status(400).json(err);
   }
 });
@@ -45,7 +46,7 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
 
@@ -54,11 +55,11 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.post('/logout', (req, res) => {
+router.get('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
-      res.status(204).end();
-    }); 
+      res.redirect('/login');
+    });
   } else {
     res.status(404).end();
   }
--- public/js/login.js
+++ public/js/login.js
@@ -1,35 +1,35 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      // Send a POST request to the API endpoint
-      const response = await $.post('/api/users/login', { email, password });
-      document.location.replace('/');  
-    }
-  };
-  
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (name && email && password) {
-      const response = await $.post('/api/users/register', { name, email, password });
-      (response) ? document.location.replace('/') : alert("Server Error. Sorry!");
-    }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
-  
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await $.post('/api/users/login', { email, password });
+    document.location.replace('/');
+  }
+};
+
+//wrap in a try catch
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await $.post('/api/users/register', { name, email, password });
+    (response) ? document.location.replace('/') : alert("Server Error. Sorry!");
+  }
+};
+// 
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
