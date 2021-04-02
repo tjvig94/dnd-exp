e52565dc69fab4081b24ce5fb896ed1b8f866df6
Tim Vigneau <tjvig94@gmail.com>
2021-04-02 16:16:48 -0400

    updated db url

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ let sequelize;
 
 if (process.env.DATABASE_URL) {
     sequelize = new Sequelize({
-      connectionString: process.env.DATABASE_URL,
+      connectionString: 'postgres://sjlumezjrothdj:23f81cc3a0fff9e6c32177231da97e1b9862da5d55e35816f94622b3bc6d5edd@ec2-52-45-73-150.compute-1.amazonaws.com:5432/damsm5mt3evnun',
       dialect: 'postgres',
       ssl: {
         rejectUnauthorized: false
