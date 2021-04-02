4c01fab995ac7425686029507c38da6d4056ccfc
Tim Vigneau <tjvig94@gmail.com>
2021-04-02 15:39:55 -0400

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,7 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DATABASE_URL) {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'postgres'
-    });
+    sequelize = new Sequelize(process.env.DATABASE_URL);
   } else {
     sequelize = new Sequelize(
       process.env.DB_NAME,
